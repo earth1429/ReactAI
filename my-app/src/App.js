48340cbda3d66c6860019e214ebec83deb133a70
Earth <kitikun.1429@mail.kmutt.ac.th>
2021-11-15 22:55:26 +0700

    [Update] Upload button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {AppBar,Toolbar,Typography} from '@mui/material/';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import { green, orange, purple, yellow } from '@mui/material/colors';
+import Box from '@mui/material/Box';
 
 function App() {
+
+
+  const Input = styled('input')({
+    display: 'none',
+  });
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: orange[500],
+    '&:hover': {
+      backgroundColor: orange[700],
+    },
+  }));
+
+  const ColorButton2 = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <div style={{ width: '100%' }}>
+     <AppBar position="static">
+  <Toolbar variant="dense">
+    <Typography variant="h4" color="inherit" component="div">
+      AI Classification Image 
+    </Typography>
+  </Toolbar>
+</AppBar>
+    </div>
+    <div className='App-header'>
+    <Stack direction="row" alignItems="center" spacing={3}>
+    <label htmlFor="contained-button-file">
+        <Input accept="image/*" id="contained-button-file" multiple type="file" />
+        <ColorButton variant="contained" component="span" size="large">
+          Upload Image
+        </ColorButton>
+      </label>
+      <label>
+      <ColorButton2 variant="contained" size="large">Classify Image</ColorButton2>
+        </label>
+      </Stack>
+      <div>
+      <Box
+        component="span"
+        sx={{
+          display: 'block',
+          p: 1,
+          m: 1,
+        }}
+      >
+        Confident Level:
+      </Box>
+      <Box
+        component="span"
+        sx={{
+          display: 'block',
+          p: 1,
+          m: 1,
+        }}
+      >
+        Type:
+      </Box>
+      </div>
+    </div>
     </div>
   );
 }
