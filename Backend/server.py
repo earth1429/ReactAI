1c00d809d58ccf56aa5f014d59ae88dae485ad4a
Earth <kitikun.1429@mail.kmutt.ac.th>
2021-11-22 18:09:31 +0700

    [Upload] Final WebApp

--- Backend/server.py
+++ Backend/server.py
@@ -1,97 +1,69 @@
 from flask import Flask, flash, request, redirect, url_for
-import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # If the user does not select a file, the browser submits an
-    # empty file without a filename.
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        print(file)
-        try:
-            return { 'peko' : easyOCRDetection(applyContours(file))} 
-        except Exception as e:
-            print(e)
-        
-
-if __name__ == '__main__':
-    app.run()
-
-    import flask
-import cv2
 import numpy as np
 import tensorflow as tf
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
+from werkzeug.wrappers import response
+from flask_cors import CORS, cross_origin
+from werkzeug.datastructures import ImmutableMultiDict
+from PIL import Image
+import os
+
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+model_path ="DomeModelpapaya.h5"
+
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config["DEBUG"] = True
+CORS(app)
+
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 def translate(x):
     return {
-        1: 'This papaya is medium!',
-        2: 'This papaya is ripe!',
-        3: 'This papaya is unripe!',
-        4: 'error'
-    }.get(x, 4)
+        0: "It's Medium",
+        1: "It's Ripe",
+        2: "It's unripe",
+        3: 'error'
+    }.get(x, 3)
 
-@app.route('/api', methods=['GET'])
-@cross_origin()
+@app.route('/test', methods=['GET'])
 def test():
     return {'test' : 'success'}
 
 
 # A route to return all of the available entries in our catalog.
-@app.route('/api/predict', methods=['POST'])
+
+
+@app.route('/upload', methods=['POST'])
 @cross_origin()
 def upload_file():
-    # data = request.files['file'].stream.read()
-    data = request.files['file']
-    data.save('C:\Users\KITIKUNPINYOPORN\Desktop\ReactAI\ReactAI\Backend\Image\papaya.png')
-    result = predict()
-    return {'payload' : result}
+    data = request.files['image']
+    data.save('./Image/papaya.png')
+    print(request.files['image'])
+    
+ #   result = predict()
+    return {'payload' : '0'}
 
-
+@app.route('/predict', methods=['GET'])
+@cross_origin()
 def predict():
-    model = tf.keras.models.load_model('C:\Users\KITIKUNPINYOPORN\Desktop\ReactAI\DomeModelpapaya.h5')
-    result = []
-    img = cv2.imread('./static/papaya.png')
-    img = cv2.resize(img,(128,128))
-    img = img / 255.
-    img= img.reshape(128,128,3)
-    result.append(img)
-    result = np.array(result)
-    classes = model.predict(result)
-    # print(classes)
-    classes = np.array(classes)
-    status = ''
-    if (np.argmax(classes)==1):
-      status = 'This papaya is medium'
-    elif (np.argmax(classes)==2):
-      status = 'This papaya is ripe'
-    else:
-      status = 'This papaya is unripe'
-    return { 'status': status }
-app.run()
+    model = tf.keras.models.load_model(model_path)
+    img = image.load_img('./Image/papaya.png', target_size=(128, 128, 3))
+    x = image.img_to_array(img) / 255.0
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    classes = model.predict(images)
+    score = tf.nn.softmax(classes[0])
+    print("Status: ", translate(np.argmax(classes[0])))
+    print("Confident Level: ", np.max(classes[0])*100)
+    
+    return {"Status": translate(np.argmax(classes[0])),"ConfidentLevel": np.max(classes[0])*100}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,57 @@
-import './App.css';
-import * as React from 'react';
-import {AppBar,Toolbar,Typography} from '@mui/material/';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-import { green, orange, purple} from '@mui/material/colors';
-import Box from '@mui/material/Box';
-import {useState, useEffect} from 'react';
+import "./App.css";
+import * as React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material/";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import { green, orange, purple } from "@mui/material/colors";
+import Box from "@mui/material/Box";
+import { useState, useEffect } from "react";
+import Axios from 'axios';
+import { render } from "react-dom";
 
-function App() {
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello1, {this.props.name}</h1>;
+  }
+}
 
+function App() { 
+  function Welcome(props) {
+    return <h1>Hello2, {result.toStr}</h1>;
+  }
   //Test API
-  const[data, setData] = useState([{}])  
+/*  const [upload, setUpload] = useState([{}]);
   useEffect(() => {
-    fetch("/upload").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+    fetch("http://localhost:5000/upload")
+      .then((res) => res.json())
+      .then((data) => {
+        setUpload(upload);
+        console.log(upload);
+      });
+  }, []);
 
-  const [image,setImage] = React.useState('/Image.png')
-  const Input = styled('input')({
-    display: 'none',
+  const [predict, setPredict] = useState([{}]);
+  useEffect(() => {
+    fetch("http://localhost:5000/predict")
+      .then((res) => res.json())
+      .then((data) => {
+        setPredict(predict);
+        console.log(upload);
+      });
+  });*/
+
+  const [state, updateState] = React.useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+  const [image, setImage] = React.useState("/Image.png");
+  const [result, setResult] = React.useState('');
+  const Input = styled("input")({
+    display: "none",
   });
   const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(purple[500]),
     backgroundColor: orange[500],
-    '&:hover': {
+    "&:hover": {
       backgroundColor: orange[700],
     },
   }));
@@ -38,68 +59,103 @@ function App() {
   const ColorButton2 = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(purple[500]),
     backgroundColor: green[500],
-    '&:hover': {
+    "&:hover": {
       backgroundColor: green[700],
     },
   }));
 
-
-  const onImageChange = event => {
-    console.log(event.target.files)
+  const onImageChange = async (event) => {
+    console.log(event.target.files);
     if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
-      setImage(URL.createObjectURL(img))
+     // let fileExt = img['name'].split('.').pop();
+     // const myRenamedFile = new File([img], create_UUID()+ "."+fileExt);
+      let data = new FormData();
+      data.append('image', img)
+
+      console.log(data);
+      setImage(URL.createObjectURL(img));
+      await Axios.post('http://localhost:5000/upload', data, {
+      headers: { "Content-Type": "multipart/form-data" }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
     }
   };
-  
-  return (
-    <div className='App'>
-    <div style={{ width: '100%' }}>
-     <AppBar position="static">
-  <Toolbar variant="dense">
-    <Typography variant="h4" color="inherit" component="div">
-      AI Classification Image 
-    </Typography>
-  </Toolbar>
-</AppBar>
-    </div>
-    <div className='App-header'>
-      <img src ={image} height='350px' width='450px'/>
-      <div style={{ height: '20px' }}/>
-     <Stack direction="row" alignItems="center" spacing={3}>
-     <label htmlFor="contained-button-file">
-        <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={onImageChange}/>
-        <ColorButton variant="contained" component="span" size="large">
-          Upload Image
-        </ColorButton>
-      </label>
-      <label>
-      <ColorButton2 variant="contained" size="large">Classify Image</ColorButton2>
-        </label>
-      </Stack>
+
+  const getResult = async (event) => {
+    await Axios.get('http://localhost:5000/predict')
+    .then(function (response){
+      setResult(response.data)
+      console.log(response.data)
+    }
+    );
+  }
+  function RenderResult() {
+    return (
       <div>
-      <Box
-        component="span"
-        sx={{
-          display: 'block',
-          p: 1,
-          m: 1,
-        }}
-      >
-        Confident Level:
-      </Box>
-      <Box
-        component="span"
-        sx={{
-          display: 'block',
-          p: 1,
-          m: 1,
-        }}
-      >
-        Type:
-      </Box>
+          <Box
+            component="span"
+            sx={{
+              display: "block",
+              p: 1,
+              m: 1,
+            }}
+          >
+            Confident Level: {result.ConfidentLevel}
+          </Box>
+          <Box
+            component="span"
+            sx={{
+              display: "block",
+              p: 1,
+              m: 1,
+            }}
+          >
+            Type: {result.Status}
+          </Box>
+        </div>
+    )
+  }
+  return (
+    <div className="App">
+      <div style={{ width: "100%" }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h4" color="inherit" component="div">
+              AI Classification Image
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
-    </div>
+      <div className="App-header">
+        <img src={image} height="350px" width="450px" />
+        <div style={{ height: "20px" }} />
+        <Stack direction="row" alignItems="center" spacing={3}>
+          <label htmlFor="contained-button-file">
+            <Input
+              accept="image/*"
+              id="contained-button-file"
+              multiple
+              type="file"
+              onChange={onImageChange}
+            />
+            <ColorButton variant="contained" component="span" size="large">
+              Upload Image
+            </ColorButton>
+          </label>
+          <label>
+            <ColorButton2 variant="contained" size="large" onClick={getResult}>
+              Classify Image
+            </ColorButton2>
+          </label>
+        </Stack>
+        <div>{(result=='') ? '': <RenderResult Status={result.Status} ConfidentLevel={result.ConfidentLevel}/>}</div>
+      </div>
     </div>
   );
 }
